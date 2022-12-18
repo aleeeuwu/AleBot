0279725bc709a4b41ec3271ed0d10fa7f3ca2686
Cood <102889231+Cood7@users.noreply.github.com>
2022-12-17 18:42:43 -0600

    Delete pikmin.py
    gonna put it in classicCommands.py

--- botcmds/pikmin.py
+++
@@ -1,8 +0,0 @@
-from discord.ext import commands
-
-@commands.command(description="rock pikmin")
-async def pikmin(ctx):
-    await ctx.send('https://cdn.discordapp.com/attachments/607657189003493376/1053821556624740423/rockPikmin.jpeg')
-    
-async def setup(bot):
-    bot.add_command(pikmin)
