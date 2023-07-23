00d5bd9b6d474c60db4bd2881eade665c7319c61
aleeeuwu <120433474+aleeeuwu@users.noreply.github.com>
2023-07-23 01:58:57 -0400

    WIP 2: gilbonus

--- botcmds/gilbonus.py
+++ botcmds/gilbonus.py
@@ -1,14 +1,34 @@
 import discord
 from discord.ext import commands
 import time
+import json
+import os
 
 async def bonus_check(guild_id: int, user_id: int):
-    # CODE FOR BONUS CHECK FUNCTION GOES HERE
+    
     print('Not implemented: bonus check')
     return
 
 @commands.hybrid_command(description='Activate the Gilbert multiplier to temporarily double your points earned from a guess!')
 async def gilbonus(ctx):
+    
+    #Checks to see if there's a scoreboard wins file for the server
+    if not os.path.exists("scoreboards/wins/" + str(ctx.guild.id) + ".json"):
+        await ctx.send("The wins scoreboard is currently empty")
+        return
+    
+    #Reads the scoreboard file for the server
+    else:
+        wins = {}
+        with open("scoreboards/wins/" + str(ctx.guild.id) + ".json", "r") as o:
+            wins = json.loads(o.read())
+            o.close()
+
+    #Checks if the user is in the file
+    if str(ctx.author.id) not in wins.keys():
+        await ctx.send("You currently do not have any Gilpoints in this server")
+        return
+
     class Confirm(discord.ui.View):
         @discord.ui.button(label='Activate', style=discord.ButtonStyle.green)
         async def confirm(self, interaction, button):
@@ -26,8 +46,13 @@ async def gilbonus(ctx):
     
     # If the button was pressed...
     if view.value:
+        if ctx.guild.id not in bonuses.keys():
+            bonuses[ctx.guild.id] = {}
+        bonuses[ctx.guild.id][ctx.author.id] = int(time.time())
         # CODE TO ACTIVATE MULTIPLIER GOES HERE
         print('[' + time.asctime() + ']', 'User ID', ctx.author.id, 'in guild ID', ctx.guild.id, 'activated Gilbert multiplier.')
 
 async def setup(bot):
     bot.add_command(gilbonus)
+    global bonuses
+    bonuses = {}
